9a349dfe19e0e4b5b83120576ca50a1fa03ecdac
throne <contact@throne.work>
2025-04-01 17:50:13 -0700

    navbar

---
+++ assets/throne.js
@@ -0,0 +1,13 @@
+(function($) {
+
+    $('.login-trigger').on('click', function() {
+
+        $('.login-dropdown').css({
+            display: 'block',
+            position: 'absolute',
+            bottom: '-100'
+        })
+
+    });
+
+})(jQuery);
